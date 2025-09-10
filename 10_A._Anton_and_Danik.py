27c4678f0016db5bdf2761366df9ea3d88d95e2d
shanto roy <shantosr3559@gmail.com>
2025-09-10 20:31:53 +0600

    A. Anton and Danik

---
+++ 10_A._Anton_and_Danik.py
@@ -0,0 +1,16 @@
+n=int(input())
+x=input()
+count1=0
+count2=0
+for i in x:
+  if i=="A":
+   count1+=1
+  elif i=="D":
+    count2+=1
+if count1>count2:
+  print("anton")
+elif count2>count1:
+  print("Danik")
+else:
+  print("Friendship")  
+     
