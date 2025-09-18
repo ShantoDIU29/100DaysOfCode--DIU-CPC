ab5bba52b0ac808553f1bd9c00605331cac925ad
shanto roy <shantosr3559@gmail.com>
2025-09-18 23:52:32 +0600

    A. Pangram

---
+++ 17_A.is_your_horseshoe_on_the_other_hoof.py
@@ -0,0 +1,3 @@
+s=list(map(int,input().split()))
+distinict=len(set(s))
+print(4-distinict)
---
+++ 18_A._Pangram.py
@@ -0,0 +1,7 @@
+n=int(input())
+s=input()
+letter=set(s.lower())
+if len(letter)==26:
+  print("YES")
+else:
+  print("NO")  
